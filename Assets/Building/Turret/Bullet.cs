96b4c6a2c385d98c39ed487acf49c0a917df7fe8
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-10-16 18:19:49 -0700

    Fix bullet hitting turrets

--- Assets/Building/Turret/Bullet.cs
+++ Assets/Building/Turret/Bullet.cs
@@ -3,27 +3,14 @@
 public class Bullet : MonoBehaviour
 {
     public ParticleSystem hitEffect;
-    public Transform bullet;
 
-    private void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Physics2D.IgnoreCollision(bullet.GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Apply knockback force to enemy entity
-        GameObject other = collision.gameObject;
-        if (other.tag == "Enemy")
+        if (other.tag != "Defense" && other.tag != "Bullet")
         {
-            other.GetComponent<Rigidbody2D>().AddForce(other.transform.position * -3f);
+            Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
         }
-
-        Debug.Log(collision.gameObject.name);
-
-        Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(gameObject);
-
     }
 
 

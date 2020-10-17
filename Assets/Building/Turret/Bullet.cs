f85529748932f9e3693dae2187eb5a43952b9283
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-10-17 01:47:51 -0700

    Change bullet behaviour

--- Assets/Building/Turret/Bullet.cs
+++ Assets/Building/Turret/Bullet.cs
@@ -4,10 +4,16 @@ public class Bullet : MonoBehaviour
 {
     public ParticleSystem hitEffect;
 
+    // hit.collider.gameObject.GetComponent<SpaceThing>().Damage(10,0);
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag != "Defense" && other.tag != "Bullet")
         {
+            if (other.name == "Triangle(Clone)")
+            {
+                other.GetComponent<TriangleAI>().TakeDamage(1);
+            }
             Instantiate(hitEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }

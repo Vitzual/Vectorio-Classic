6978a7af5eb244f3de87d3410c90462d832a1097
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-10-17 17:16:24 -0700

    Add camera controller

---
+++ Assets/Camera/CameraMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Declare variables
+    protected float moveSpeed = 20f;
+    protected new Rigidbody2D camera;
+
+    protected Vector2 movement;
+    protected Vector2 mousePos;
+
+    private void Start()
+    {
+        camera = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Get directional movement
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed = 50f;
+        } else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = 30f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move player
+        camera.MovePosition(camera.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}

a40c71b8826e3258c7a0ae776235c701fed49619
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-10-18 17:51:03 -0700

    Added Anti-Aliasing in project view

---
+++ Assets/Building/Bolt/Bolt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Bolt : BulletClass
+{
+
+    public ParticleSystem Effect;
+
+    public void Start()
+    {
+        HitEffect = Effect;
+        damage = 1;
+        StartCoroutine(SetLifetime(1));
+    }
+
+    public override void collide()
+    {
+        Instantiate(HitEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Building/Bolt/BoltAI.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class BoltAI : TurretDefense
+{
+    // Turret AI variables 
+    public Transform Point;
+    public Rigidbody2D Gun;
+    public GameObject Bullet;
+
+    // On start, assign weapon variables
+    void Start()
+    {
+        fireRate = 0.5f;
+        bulletForce = 100f;
+        bulletSpread = 0.1f;
+        bulletAmount = 3;
+        range = 1000;
+    }
+
+    // Targetting system
+    void Update()
+    {
+        // Find closest enemy 
+        var target = EnemyPool.FindClosestEnemy(transform.position, range);
+
+        // If a target exists, shoot at it
+        if (target != null)
+        {
+            // Rotate turret towards target
+            Vector2 TargetPosition = new Vector2(target.gameObject.transform.position.x, target.gameObject.transform.position.y);
+            Vector2 lookDirection = (TargetPosition - Gun.position);
+            float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+
+            // Smooth rotation when targetting enemies
+            if (Gun.rotation >= angle && !((Gun.rotation - angle) <= 0.3 && (Gun.rotation - angle) >= -0.3))
+            {
+                Gun.rotation -= 0.3f;
+            } 
+            else if (Gun.rotation <= angle && !((Gun.rotation - angle) <= 0.3 && (Gun.rotation - angle) >= -0.3))
+            {
+                Gun.rotation += 0.3f;
+            }
+            
+            // If turret is pointing at target, fire at it
+            if ((Gun.rotation - angle) <= 1 && (Gun.rotation - angle) >= -1)
+            {
+                // Call shoot function
+                Shoot(Bullet, Point);
+            }
+        }
+    }
+}

ddb7e53fb97d086bb3eec869d483aee3a1a6c320
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-11-23 15:13:12 -0800

    Make grid dissapear at far range

--- Assets/Camera/CameraScroll.cs
+++ Assets/Camera/CameraScroll.cs
@@ -7,6 +7,8 @@ public class CameraScroll : MonoBehaviour
     private float zoomFactor = 10f;
     [SerializeField]
     private float zoomSpeed = 20;
+    private bool gridActive = true;
+    public GameObject grid;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +26,16 @@ public class CameraScroll : MonoBehaviour
         targetZoom -= scrollData * zoomFactor;
         targetZoom = Mathf.Clamp(targetZoom, 5f, 150f);
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomSpeed);
+
+        if (targetZoom >= 100f && gridActive == true)
+        {
+            grid.SetActive(false);
+            gridActive = false;
+        } 
+        else if (targetZoom < 100f && gridActive == false)
+        {
+            grid.SetActive(true);
+            gridActive = true;
+        }
     }
 }

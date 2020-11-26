e363f5ee6cbb68e87ee57727082b6b05f652873d
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-11-26 13:19:08 -0800

    Add enhancer buildings

---
+++ Assets/Building/Power/Enhancer/Projector.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Projector : TileClass
+{
+    // Internal placement variables
+    [SerializeField] private LayerMask TileLayer;
+    public Transform rotator;
+
+    private void Start()
+    {
+        var colliders = Physics2D.OverlapBoxAll(this.gameObject.transform.position, new Vector2(7,7), 1 << LayerMask.NameToLayer("Defense"));
+        for (int i=0; i<colliders.Length; i++)
+        {
+            if (colliders[i].name.Contains("Collector"))
+            {
+                colliders[i].GetComponent<CollectorAI>().increaseAmount(4);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotator.Rotate(Vector3.forward, 50f * Time.deltaTime);
+    }
+
+    // Kill defense
+    public override void DestroyTile()
+    {
+        var colliders = Physics2D.OverlapBoxAll(this.gameObject.transform.position, new Vector2(5, 5), 1 << LayerMask.NameToLayer("Defense"));
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].name.Contains("Collector"))
+            {
+                colliders[i].GetComponent<CollectorAI>().decreaseAmount(4);
+            }
+        }
+
+        Instantiate(Effect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
--- Assets/Building/World/Collector/CollectorAI.cs
+++ Assets/Building/World/Collector/CollectorAI.cs
@@ -4,11 +4,21 @@ public class CollectorAI: TileClass
 {
     // Declare local object variables
     public int amount;
+    public bool enhanced;
     private GameObject SRVSC;
 
     // On start, invoke repeating SendGold() method
     private void Start()
     {
+        var colliders = Physics2D.OverlapBoxAll(this.gameObject.transform.position, new Vector2(7, 7), 1 << LayerMask.NameToLayer("Defense"));
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].name.Contains("Enhancer"))
+            {
+                increaseAmount(4);
+            }
+        }
+
         SRVSC = GameObject.Find("Survival");
         InvokeRepeating("SendGold", 0f, 1f);
     }
@@ -18,6 +28,18 @@ public class CollectorAI: TileClass
     {
         SRVSC.GetComponent<Survival>().AddGold(amount);
         SRVSC.GetComponent<Survival>().UpdateGui();
+    }
+
+    // Increase gold
+    public void increaseAmount(int a)
+    {
+        amount += a;
+    }
+
+    // Decrease gold
+    public void decreaseAmount(int a)
+    {
+        amount -= a;
     }
 
     // Kill defense

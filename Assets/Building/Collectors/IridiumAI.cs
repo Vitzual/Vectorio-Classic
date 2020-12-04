27637361d145c2db1a55c011f8bd274821b73bf4
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-12-03 22:22:26 -0800

    Added Iridium Mine

---
+++ Assets/Building/Collectors/IridiumAI.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class IridiumAI: TileClass
+{
+    // Declare local object variables
+    public int amount;
+    public bool enhanced;
+    private GameObject SRVSC;
+
+    // On start, invoke repeating SendGold() method
+    private void Start()
+    {
+        SRVSC = GameObject.Find("Survival");
+        InvokeRepeating("SendIridium", 0f, 1f);
+    }
+
+    // Send gold
+    private void SendIridium()
+    {
+        if (enhanced) SRVSC.GetComponent<Survival>().AddIridium(amount * 4);
+        else SRVSC.GetComponent<Survival>().AddIridium(amount);
+    }
+
+    // Increase gold
+    public void doubleAmount()
+    {
+        amount = amount * 2;
+    }
+
+    // Enhance collector
+    public void enhanceCollector()
+    {
+        enhanced = true;
+    }
+
+    // Deenhance collector
+    public void deenhanceCollector()
+    {
+        enhanced = false;
+    }
+
+    // Kill defense
+    public override void DestroyTile()
+    {
+        GameObject.Find("Survival").GetComponent<Survival>().decreasePowerConsumption(power);
+        Instantiate(Effect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}

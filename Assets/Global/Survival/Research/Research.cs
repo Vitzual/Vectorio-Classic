b53c8d207c6c7f34e6339f207d9d4168409012d7
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2020-12-27 16:28:13 -0500

    Add unlock research method

--- Assets/Global/Survival/Research/Research.cs
+++ Assets/Global/Survival/Research/Research.cs
@@ -16,22 +16,42 @@ public class Research : MonoBehaviour
     public Image ResearchImage;
     public int ResearchLevel;
 
+    // Data source scripts
+    public Survival SurvivalCS;
+
     // Research array class
     [System.Serializable]
-    public class Researchables
+    public class Researchable
     {
         public GameObject ResearchObject;
-        public int EssenceRequired;
-        public int ResearchNeeded;
-        public string ResearchTitle;
-        [TextArea] public string ResearchDescription;
-        public ButtonManagerBasicIcon ResearchButton;
+        public int EssenceRequired; // The cost of the research
+        public int[] RequiredResearch; // Array of indexs of the required research nodes
+        public string Title; // The title to be displayed in the research menu
+        [TextArea] public string Description; // The description to be displayed in the research menu
+        public ButtonManagerBasicIcon ResearchButton; // The button associated with this research
         public ButtonManagerBasicIcon ResearchInvButton;
+        public bool IsResearched; // If this researchable is researched
     }
 
-    // Research status tracking
-    public Researchables[] ResearchTier;
-    public bool[] Researched;
+    // Research tracking
+    public Researchable[] Researchables;
+
+    public bool UnlockResearch(int i)
+    {
+        Researchable research = Researchables[i]; // Get research as a temp variable
+
+        foreach (int j in research.RequiredResearch) // Returns false if one or more required research is not researched
+            if (!Researchables[j].IsResearched) return false;
+
+        if (research.EssenceRequired < SurvivalCS.essence) return false; // Return false if the player does not have the required essence
+
+        SurvivalCS.essence -= research.EssenceRequired; // Subtract the cost from SurvivalCS
+        research.IsResearched = true; // Set research to researched
+
+        Researchables[i] = research; // Store temp variable
+
+        return true;
+    }
 
     public void ShowResearchButton()
     {

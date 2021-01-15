8af8ea5fad7e41588f6d5d0555ce08cd0c5f5098
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-01-14 22:32:03 -0800

    Re-organized shaders / Added main pipeline

---
+++ Assets/Shaders/Dissolve/DissolveSphere.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveSphere : MonoBehaviour {
+
+    Material mat;
+
+    private void Start() {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    private void Update() {
+        mat.SetFloat("_DissolveAmount", Mathf.Sin(Time.time) / 2 + 0.1f);
+    }
+}

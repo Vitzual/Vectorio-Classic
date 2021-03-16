360ed560f42ed8385f34892e6cf5599ec37a10f2
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-03-16 14:07:26 -0700

    Added epilepsy warning

---
+++ Assets/Scenes/OpenMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class OpenMenu : MonoBehaviour
+{
+    public CanvasGroup thing;
+    public int delay = 500;
+    public float adjuster = 1f;
+
+    public void Start()
+    {
+        InvokeRepeating("Lower", 0f, 0.01f);
+    }
+
+    public void Lower()
+    {
+        if (delay == 0)
+        {
+            adjuster -= 0.01f;
+            thing.alpha = adjuster;
+            if (adjuster <= 0f)
+                SceneManager.LoadScene("Menu");
+        }
+        else delay -= 1;
+        
+    }
+}

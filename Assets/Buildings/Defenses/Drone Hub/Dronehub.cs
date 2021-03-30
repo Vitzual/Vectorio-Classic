ec8fa60832b6ded53d1838bbdf70606c62043009
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-03-29 17:00:41 -0700

    Drone hub models

---
+++ Assets/Buildings/Defenses/Drone Hub/Dronehub.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dronehub : TileClass
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Kill defense
+    public override void DestroyTile()
+    {
+        Survival srv = GameObject.Find("Survival").GetComponent<Survival>();
+        srv.decreasePowerConsumption(power);
+        TurretHandler.buildings.Remove(transform);
+        GameObject.Find("Spawner").GetComponent<WaveSpawner>().decreaseHeat(heat);
+        Instantiate(Effect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}

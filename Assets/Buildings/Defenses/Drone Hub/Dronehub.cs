6f2ed044b69548b2dd32a894012bee14b09e2dd4
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-03-29 21:32:59 -0700

    Added tester drone model

--- Assets/Buildings/Defenses/Drone Hub/Dronehub.cs
+++ Assets/Buildings/Defenses/Drone Hub/Dronehub.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Dronehub : TileClass
 {
+    // Side panels 
+    Transform leftPanel;
+    Transform rightPanel;
+
     // Start is called before the first frame update
     void Start()
     {

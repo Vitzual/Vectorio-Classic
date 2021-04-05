706015904a5b056623b7cd235620e701023cf1b8
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-04-04 17:14:09 -0700

    Linked drone generation to drone handler

--- Assets/Buildings/Defenses/Drone Hub/Dronehub.cs
+++ Assets/Buildings/Defenses/Drone Hub/Dronehub.cs
@@ -10,9 +10,14 @@ public class Dronehub : TileClass
     // Drone variables
     public bool droneActive = false;
     public int checkDrone = 0;
+    public int droneType;
 
     // Hold drone type
-    public Transform droneType;
+    public Transform activeDrone;
+    public Transform fixerDrone;
+    public Transform resourceDrone;
+    public Transform attackDrone;
+    public Transform engineerDrone;
 
     // Side panels 
     public Transform leftPanel;
@@ -25,7 +30,45 @@ public class Dronehub : TileClass
     // Start is called before the first frame update
     void Start()
     {
+        droneManager = GameObject.Find("Drone Handler").GetComponent<DroneManager>();
         checkDrone = Random.Range(0, 100);
+    }
+
+    public void setType(int a)
+    {
+        droneType = a;
+    }
+
+    public void spawnDrone()
+    {
+        switch(droneType)
+        {
+            case 0:
+                activeDrone = Instantiate(fixerDrone, transform.position, Quaternion.identity);
+                activeDrone.name = fixerDrone.name;
+                droneManager.registerFixerDrone(activeDrone, 2, 10, 1, 10);
+                break;
+            case 1:
+                activeDrone = Instantiate(resourceDrone, transform.position, Quaternion.identity);
+                activeDrone.name = resourceDrone.name;
+                // REGISTER DRONE HERE (WITH VARIABLES)
+                break;
+            case 2:
+                activeDrone = Instantiate(attackDrone, transform.position, Quaternion.identity);
+                activeDrone.name = attackDrone.name;
+                // REGISTER DRONE HERE (WITH VARIABLES)
+                break;
+            case 3:
+                activeDrone = Instantiate(engineerDrone, transform.position, Quaternion.identity);
+                activeDrone.name = engineerDrone.name;
+                // REGISTER DRONE HERE (WITH VARIABLES)
+                break;
+            default:
+                activeDrone = Instantiate(fixerDrone, transform.position, Quaternion.identity);
+                activeDrone.name = fixerDrone.name;
+                droneManager.registerFixerDrone(activeDrone, 2, 10, 1, 10);
+                break;
+        }
     }
 
     public void Update()
@@ -33,6 +76,7 @@ public class Dronehub : TileClass
         if (checkDrone == 100)
         {
             // Check if drone active, if not, spawn it
+            isAnimating = true;
 
             checkDrone = 0;
         }
@@ -63,13 +107,6 @@ public class Dronehub : TileClass
             isAnimating = false;
             animSpeed = 1f;
         }
-    }
-
-    public void SpawnDrone()
-    {
-        var holder = Instantiate(droneType, transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
-        holder.name = droneType.name;
-
     }
 
     // Kill defense

c5c69a1e87c068a849770bf533f3a00902c3768c
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-06-19 20:40:25 -0700

    Disintegrator finished

---
+++ Assets/Buildings/Defenses/Disintegrator/DisintegratorAI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class DisintegratorAI : TurretClass
+{
+    // Targetting system
+    void Update()
+    {
+        // If animation is playing, wait
+        if (animPlaying)
+        {
+            PlayAnim();
+            return;
+        }
+
+        if (isRotating)
+            RotationHandler();
+
+        // If a target exists, shoot at it
+        if (target != null && !isRotating)
+        {
+            // Unflag hasTarget
+            hasTarget = false;
+                
+            // Call shoot function
+            Shoot(Bullet, FirePoints[0]);
+        } else {
+            // Unflag hasTarget when target is null
+            hasTarget = false;
+        }
+    }
+
+    // Kill defense
+    public override void DestroyTile()
+    {
+        Survival srv = GameObject.Find("Survival").GetComponent<Survival>();
+        srv.decreasePowerConsumption(power);
+        BuildingHandler.buildings.Remove(transform);
+        GameObject.Find("Spawner").GetComponent<WaveSpawner>().decreaseHeat(heat);
+        Instantiate(Effect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}

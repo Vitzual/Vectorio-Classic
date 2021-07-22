a46153e05c98f9d8e4dc8c24fe0255a95a97608a
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-07-21 21:22:10 -0700

    Create IDamageable variables / methods

--- Assets/Scripts/Interfaces/IDamageable.cs
+++ Assets/Scripts/Interfaces/IDamageable.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// IDamageable Interface
+//
+// Apply to entities that should be able to receive damage
 
-public class IDamageable : MonoBehaviour
+public interface IDamageable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    int health { get; set; }
+    int maxHealth { get; set; }
+    void damageEntity(int dmg);
+    void destroyEntity();
+    void healEntity(int amounnt);
 }

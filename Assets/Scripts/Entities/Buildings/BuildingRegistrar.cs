97db5c54d93daa863ac2b345eea88ed74636133d
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-07-22 20:05:44 -0700

    Add TurretStats and TileStats classes to BuildingRegistrar

--- Assets/Scripts/Entities/Buildings/BuildingRegistrar.cs
+++ Assets/Scripts/Entities/Buildings/BuildingRegistrar.cs
@@ -2,8 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// The building registrar contains a list of stats for each building
+// This can be added to at run time to support in-game modding
+
 public class BuildingRegistrar : MonoBehaviour
 {
-    // Registers buildings to the tech tree
-    public List<Transform> Buildings;
+    // Contains a list of all turret stats
+    [System.Serializable]
+    public class TurretStats
+    {
+        // Turret info
+        public string name;
+        public Transform turretObj;
+
+        // Contains turret stats
+        public int damage;
+        public int range;
+        public int rotationSpeed;
+        public float fireRate;
+        public int bulletPierces;
+        public int bulletAmount;
+        public float bulletSpeed;
+        public float bulletSpread;
+    }
+
+    // Contains a list of all tile stats 
+    [System.Serializable]
+    public class TileStats
+    {
+        // Tile info
+        public string name;
+        public Transform tileObj;
+
+        // Contains tile stats
+        public int health;
+        public int maxHealth;
+        public int cost;
+        public int power;
+        public int heat;
+    }
+
+    // Create class lists
+    public List<TurretStats> turretStats;
+    public List<TileStats> tileStats;
+
+    // Grab turret stats
+    public TurretStats getTurretStats(Transform turret)
+    {
+        foreach (TurretStats stat in turretStats)
+            if (turret == stat.turretObj) return stat;
+        return null;
+    }
+
+    // Grab tile stats
+    public TileStats getTileStats (Transform tile)
+    {
+        foreach (TileStats stat in tileStats)
+            if (tile == stat.tileObj) return stat;
+        return null;
+    }
 }

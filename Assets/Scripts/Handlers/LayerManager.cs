fd2fdcd72f78d3f9ca7e50e11a91b35c0b24a8f6
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-07-22 17:36:33 -0700

    Add turret layer to LayerManager.cs

--- Assets/Scripts/Handlers/LayerManager.cs
+++ Assets/Scripts/Handlers/LayerManager.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class LayerManager : MonoBehaviour
 {
-    
+    // List of layers to get on run
+    public string turretLayerName;
+    public static LayerMask turretLayer;
+
+    private void Start()
+    {
+        turretLayer = 1 << LayerMask.NameToLayer(turretLayerName);
+    }
+
+    public static LayerMask getTurretLayer()
+    {
+        return turretLayer;
+    }
 }

3dc9c37e3caf6c52145158ab62e6798db73e72d1
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-08-22 17:25:20 -0700

    Extend survival class to support networking

--- Assets/Scripts/Survival/NewSurvival.cs
+++ Assets/Scripts/Survival/NewSurvival.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
 // This is a WIP rewrite script of Survival.cs
-public class NewSurvival : MonoBehaviour
+public class NewSurvival : NetworkBehaviour
 {
     public void Start()
     {
-        Events.active.onBuildingPlaced += BuildingPlaced;
+        Events.active.onBuildingPlaced += CmdBuildingPlaced;
     }
 
-    public void BuildingPlaced(Transform building)
+    [Command]
+    public void CmdBuildingPlaced(Transform building)
     {
         BaseBuilding script = building.GetComponent<BaseBuilding>();
 
-        if (script != null)
-        {
-            Resource.Remove(Resource.Currency.Gold, script.building.cost);
-            Resource.Remove(Resource.Currency.Power, script.building.power);
-            Resource.Remove(Resource.Currency.Heat, script.building.heat);
-        }
+        // Update resources for all clients
+        if (script != null) RpcBuildingPlaced(script.building.cost, script.building.power, script.building.heat);
         else Debug.LogError("Could not retrieve script from " + transform.name);
+    }
+
+    [ClientRpc]
+    public void RpcBuildingPlaced(int gold, int power, int heat)
+    {
+        Resource.Remove(Resource.Currency.Gold, gold);
+        Resource.Remove(Resource.Currency.Power, power);
+        Resource.Remove(Resource.Currency.Heat, heat);
     }
 }

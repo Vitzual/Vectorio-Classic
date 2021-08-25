05cc9efbd99f5ba361fb2b10a34dfdc1e7e7c89f
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-08-25 12:19:41 -0700

    Create Keybinds.cs

---
+++ Assets/Scripts/Player/Keybinds.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Keybinds
+{
+    public static KeyCode move_up = KeyCode.W;
+    public static KeyCode move_down = KeyCode.S;
+    public static KeyCode move_left = KeyCode.A;
+    public static KeyCode move_right = KeyCode.D;
+    public static KeyCode sprint = KeyCode.LeftShift;
+    public static KeyCode shoot = KeyCode.Mouse0;
+    public static KeyCode deselect = KeyCode.Mouse1;
+    public static KeyCode escape = KeyCode.Escape;
+    public static KeyCode equip = KeyCode.E;
+    public static KeyCode rotate = KeyCode.R;
+    public static KeyCode map = KeyCode.M;
+    public static KeyCode hotbar_1 = KeyCode.Alpha1;
+    public static KeyCode hotbar_2 = KeyCode.Alpha2;
+    public static KeyCode hotbar_3 = KeyCode.Alpha3;
+    public static KeyCode hotbar_4 = KeyCode.Alpha4;
+    public static KeyCode hotbar_5 = KeyCode.Alpha5;
+    public static KeyCode hotbar_6 = KeyCode.Alpha6;
+    public static KeyCode hotbar_7 = KeyCode.Alpha7;
+    public static KeyCode hotbar_8 = KeyCode.Alpha8;
+    public static KeyCode hotbar_9 = KeyCode.Alpha9;
+    public static KeyCode hotbar_0 = KeyCode.Alpha0;
+}

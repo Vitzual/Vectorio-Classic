fc763a4790d6417088f659200f42223c95a56ea1
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-09-02 19:09:46 -0700

    Create menu button object / script

--- Assets/Scripts/UI/MenuButton.cs
+++ Assets/Scripts/UI/MenuButton.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
+using Michsky.UI.ModernUIPack;
+using TMPro;
 
 public class MenuButton : MonoBehaviour
 {
-    public new TextMeshProUGUI name;
-    
+    // Building holder
+    public Building building;
+
+    // Button variables
+    public new ButtonManagerBasic button;
+    public TextMeshProUGUI desc;
+    public Image icon;
+
+    // Resource icons
+    public GameObject iridiumIcon;
+    public GameObject essenceIcon;
+    public GameObject goldIcon;
+    public GameObject heatIcon;
+    public GameObject powerIcon;
 }

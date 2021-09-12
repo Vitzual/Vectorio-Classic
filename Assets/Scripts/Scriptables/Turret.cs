da87922c5a8c5427b57a06dc1e9dd2498933aa2c
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-09-11 19:16:29 -0700

    Setup turret scriptable methods for overriding

--- Assets/Scripts/Scriptables/Turret.cs
+++ Assets/Scripts/Scriptables/Turret.cs
@@ -30,6 +30,7 @@ public class Turret : Building
     // This gets used to set the stats on the building menu panel
     public override void CreateStats(Panel panel)
     {
+        panel.CreateStat(new Stat("Health", health, healthModifier, Sprites.GetSprite("Health")));
         panel.CreateStat(new Stat("Damage", damage, damageModifier, Sprites.GetSprite("Damage")));
         panel.CreateStat(new Stat("Range", range, damageModifier, Sprites.GetSprite("Range")));
         panel.CreateStat(new Stat("Firerate", fireRate, damageModifier, Sprites.GetSprite("Firerate")));
@@ -39,5 +40,94 @@ public class Turret : Building
 
         // Base method
         base.CreateStats(panel);
+    }
+
+    public virtual void RotateTurret(TurretEntity entity)
+    {
+        // Get target position relative to this entity
+        Vector2 targetPosition = new Vector2(entity.target.position.x, entity.target.position.y);
+
+        // Get the distance from the turret to the target
+        Vector2 distance = targetPosition - new Vector2(entity.barrel.position.x, entity.barrel.position.y);
+
+        // Get the angle between the gun position and the target position
+        float targetAngle = Mathf.Atan(distance.y / distance.x) * Mathf.Rad2Deg + 90f;
+        if (distance.x > 0) targetAngle += 180;
+
+        // Correct for if target is directly above or below the turret
+        if (distance.x == 0)
+        {
+            if (distance.y > 0) targetAngle = 0;
+            else targetAngle = 180;
+        }
+
+        // Calculate the difference between the target angle and the current angle
+        float difference = targetAngle - (entity.barrel.rotation.eulerAngles.z);
+
+        if ((difference < 0 || difference >= 180) && !(difference < -180))
+        {
+            // Calculate how far to rotate the turret given how long since the last frame
+            float distanceToRotate = -entity.turret.rotationSpeed * Time.deltaTime;
+
+            // If distance to rotate would rotate past the target only rotate the distance
+            if (distanceToRotate < difference)
+                distanceToRotate = difference;
+
+            // Rotate the turret
+            entity.barrel.Rotate(Vector3.forward, distanceToRotate);
+        }
+        else if (!(difference <= 5 && difference >= -5))
+        {
+            // Calculate how far to rotate the turret given how long since the last frame
+            float distanceToRotate = -entity.turret.rotationSpeed * Time.deltaTime;
+
+            // If distance to rotate would rotate past the target only rotate the distance
+            if (distanceToRotate > difference)
+                distanceToRotate = difference;
+
+            // Rotate the turret
+            entity.barrel.Rotate(Vector3.forward, distanceToRotate);
+        }
+        else
+        {
+            entity.barrel.transform.eulerAngles = new Vector3(0, 0, targetAngle);
+            if (entity.cooldown > 0) 
+            {
+                entity.cooldown -= Time.deltaTime;
+            }
+            else
+            {
+                Shoot(entity);
+                entity.cooldown = entity.turret.fireRate;
+            }
+        }
+
+    }
+
+    // Attempts to fire a bullet and returns true if fired
+    public virtual void Shoot(TurretEntity entity)
+    {
+        foreach (Transform firePoint in entity.firePoints)
+            for (int i = 0; i < entity.turret.bulletAmount; i += 1)
+                CreateBullet(entity, firePoint.position);
+    }
+
+    // Create a bullet object
+    public virtual void CreateBullet(TurretEntity entity, Vector2 position)
+    {
+        if (entity.turret.sound != null)
+            Debug.Log("Playing sound!");
+
+        GameObject bullet = Instantiate(entity.bullet, position, entity.barrel.rotation);
+        bullet.transform.rotation = entity.barrel.rotation;
+        bullet.transform.Rotate(0f, 0f, Random.Range(-entity.turret.bulletSpread, entity.turret.bulletSpread));
+
+        float speed = Random.Range(entity.turret.bulletSpeed - 10, entity.turret.bulletSpeed + 10);
+        int pierces = entity.turret.bulletPierces + Research.research_pierce;
+        float damage = entity.turret.damage + Research.research_damage;
+
+        // Dependent on the bullet, register under the correct master script
+        Events.active.BulletFired(new Bullet(bullet.transform, entity.target, speed, pierces, damage, true));
+        Debug.Log("Broadcasting bullet fired event");
     }
 }

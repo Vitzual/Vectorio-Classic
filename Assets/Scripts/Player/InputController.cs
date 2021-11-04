0351c74cce59ac4ca96d3eabe6a12b6b1330cf24
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-11-03 18:50:47 -0700

    Add back all buildings

--- Assets/Scripts/Player/InputController.cs
+++ Assets/Scripts/Player/InputController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class InputController : MonoBehaviour
 {
     // Get the camera
     public Camera cam;

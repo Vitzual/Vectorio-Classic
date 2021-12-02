5916904c59a3a4953b46f0f1e4d36b5ca35642c5
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-12-01 20:30:33 -0800

    Port Automa player list scripts

---
+++ Assets/Scripts/Networking/JoinButton.cs
@@ -0,0 +1,65 @@
+using HeathenEngineering.SteamworksIntegration;
+using Michsky.UI.ModernUIPack;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinButton : MonoBehaviour
+{
+    public ButtonManagerBasic button;
+    public Image image;
+    [SerializeField] public TextMeshProUGUI session;
+    private UserData userData;
+    public string clientOf;
+
+    public void SetUserData(UserData userData)
+    {
+        this.userData = userData;
+    }
+
+    public void UpdateUserData()
+    {
+        button.buttonText = userData.Name;
+        //image.sprite = Sprite.Create(userData.avatar, image.sprite.rect, Vector2.zero);
+        clientOf = userData.GetRichPresenceValue("clientOf");
+
+        // Set colors based on State
+        if (clientOf != "")
+        {
+            session.text = "In Game";
+        }
+        else if (userData.GameInfo.m_gameID.AppID().Equals(SteamSettings.ApplicationId))
+        {
+            session.text = "In Menus";
+        }
+        else if (userData.State.HasFlag(Steamworks.EPersonaState.k_EPersonaStateOnline))
+        {
+            session.text = "Online";
+        }
+        else if (userData.State.HasFlag(Steamworks.EPersonaState.k_EPersonaStateAway))
+        {
+            session.text = "Away";
+        }
+        else
+        {
+            session.text = "Offline";
+        }
+
+        button.UpdateUI();
+    }
+
+    public void ConnectToUser()
+    {
+        if (userData.GameInfo.m_gameID.AppID().Equals(SteamSettings.ApplicationId))
+        {
+            print(clientOf);
+            NetworkManagerSF.active.Join(clientOf);
+        }
+        else
+        {
+            Debug.Log($"{userData.DisplayName} is not in a game!");
+        }
+    }
+}
---
+++ Assets/Scripts/Networking/PlayerList.cs
@@ -0,0 +1,84 @@
+using Michsky.UI.ModernUIPack;
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerList : MonoBehaviour
+{
+    // Network Manager
+    [SerializeField] private NetworkManager networkManager;
+
+    // Host Options
+    [SerializeField] private SliderManager maxPlayersSlider;
+
+    // Join Options
+    [SerializeField] private JoinButton joinButton;
+    [SerializeField] private Transform joinList;
+
+    public void UpdateMaxPlayers()
+    {
+        maxPlayersSlider.GetComponentInChildren<TextMeshProUGUI>().text = $"Max Friends ({(int)maxPlayersSlider.value})";
+    }
+
+    public void UpdateFriendsList()
+    {
+        List<UserData> friends = SortFriendsList(SteamSettings.Client.ListFriends());
+
+        foreach (UserData friend in friends)
+        {
+            JoinButton button = Instantiate(joinButton.gameObject, Vector3.zero, Quaternion.identity).GetComponent<JoinButton>();
+            button.SetUserData(friend);
+            button.UpdateUserData();
+            button.GetComponent<RectTransform>().localScale = new Vector3(.8f, .8f, .8f);
+            button.transform.SetParent(joinList);
+        }
+    }
+
+    private List<UserData> SortFriendsList(List<UserData> friends)
+    {
+        List<UserData> unsorted = friends;
+        List<UserData> sorted = new List<UserData>();
+
+        // Put list in alphabetical order
+        unsorted = unsorted.OrderBy(friend => friend.DisplayName).ToList();
+
+        // Get friends currently in-game
+        for (int i = 0; i < unsorted.Count; i++)
+        {
+            if (unsorted[i].GameInfo.m_gameID.AppID().Equals(SteamSettings.ApplicationId))
+            {
+                sorted.Add(unsorted[i]);
+                unsorted.Remove(unsorted[i]);
+                i--;
+            }
+        }
+
+        // Get online friends
+        for (int i = 0; i < unsorted.Count; i++)
+        {
+            if (unsorted[i].State.HasFlag(Steamworks.EPersonaState.k_EPersonaStateOnline))
+            {
+                sorted.Add(unsorted[i]);
+                unsorted.Remove(unsorted[i]);
+                i--;
+            }
+        }
+
+        // Get away friends
+        for (int i = 0; i < unsorted.Count; i++)
+        {
+            if (unsorted[i].State.HasFlag(Steamworks.EPersonaState.k_EPersonaStateAway))
+            {
+                sorted.Add(unsorted[i]);
+                unsorted.Remove(unsorted[i]);
+                i--;
+            }
+        }
+
+        // Add remaining friends
+        sorted.AddRange(unsorted);
+
+        return sorted;
+    }
+}

7742c254fb892eeed32ce9d74890c2e4c053a5ed
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-12-06 09:25:36 -0800

    Setup syncer methods

--- Assets/Scripts/Networking/Syncer.cs
+++ Assets/Scripts/Networking/Syncer.cs
@@ -7,13 +7,70 @@ using Mirror;
 
 public class Syncer : NetworkBehaviour
 {
+    public Dictionary<int, BaseEntity> active = new Dictionary<int, BaseEntity>();
+
+    // Setup new list
+    public void Setup()
+    {
+        active = new Dictionary<int, BaseEntity>();
+    }
+
     [Command]
     public void CmdSyncEntity(string id, Vector2 position, Quaternion rotation)
     {
 
     }
 
-    public void RequestMatchInfo()
+    [Command]
+    public void CmdSyncHealth(int id)
+    {
+
+    }
+
+    [Command]
+    public void CmdSyncDestroy(int id)
+    {
+
+    }
+
+    [Command]
+    public void CmdSyncMetadata(int id, int metadata)
+    {
+
+    }
+
+    [ClientRpc]
+    public void RpcSyncEntity(string id, Vector2 position, Quaternion rotation)
+    {
+
+    }
+
+    [ClientRpc]
+    public void RpcSyncHealth(int id)
+    {
+
+    }
+
+    [ClientRpc]
+    public void RpcSyncDestroy(int id)
+    {
+
+    }
+
+    [ClientRpc]
+    public void RpcSyncMetadata(int id, int metadata)
+    {
+
+    }
+
+    [ClientCallback]
+    public void CmdRequestMatchInfo()
+    {
+
+    }
+
+    [TargetRpc]
+    public void RpcRequestMatchInfo()
     {
 
     }
